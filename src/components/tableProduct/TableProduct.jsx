32a7586f2faf53f6732ba52d4c465e4c7eadf1b3
Sandra-Herrera <97549362+Sandra-Herrera@users.noreply.github.com>
2022-08-01 03:49:22 -0500

    publish button

--- src/components/tableProduct/TableProduct.jsx
+++ src/components/tableProduct/TableProduct.jsx
@@ -13,75 +13,77 @@ const TableProduct = () => {
 
   return (
     <>
-      <Table responsive>
-        <thead>
-          <tr>
-            <th className={styles.theadTitle}>
-              NOMBRE DE PRODUCTO
-              <button className={styles.sortButton}>
-                <Icon icon="carbon:chevron-sort" width="20" height="30" />{" "}
-              </button>
-            </th>
-            <th className={styles.theadTitle}>
-              SKU SIMPLE
-              <button className={styles.sortButton}>
-                <Icon icon="carbon:chevron-sort" width="20" height="30" />{" "}
-              </button>
-            </th>
-            <th className={styles.theadTitle}>
-              SKU
-              <button className={styles.sortButton}>
-                <Icon icon="carbon:chevron-sort" width="20" height="30" />{" "}
-              </button>
-            </th>
-            <th className={styles.theadTitle}>
-              UPC
-              <button className={styles.sortButton}>
-                <Icon icon="carbon:chevron-sort" width="20" height="30" />{" "}
-              </button>
-            </th>
-            <th className={styles.theadTitle}>
-              PRECIO
-              <button className={styles.sortButton}>
-                <Icon icon="carbon:chevron-sort" width="20" height="30" />{" "}
-              </button>
-            </th>
-            <th className={styles.theadTitle}>
-              FECHA DE CARGA
-              <button className={styles.sortButton}>
-                <Icon icon="carbon:chevron-sort" width="20" height="30" />{" "}
-              </button>
-            </th>
-            <th className={styles.theadTitle}>
-              ESTADO
-              <button className={styles.sortButton}>
-                <Icon icon="carbon:chevron-sort" width="20" height="30" />{" "}
-              </button>
-            </th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {products.map((product, index) => {
-            return (
-              <tr key={index}>
-                <td>{product.product_name}</td>
-                <td>{product.simple_sku}</td>
-                <td>{product.sku} </td>
-                <td>{product.upc}</td>
-                <td>{product.price}</td>
-                <td>{Moment(new Date(product.created_at)).format('DD/MM/YYYY')}</td>
-                <td>
-                  <button className={styles.publishedButton}>Publicado</button>
-                </td>
-                <td>
-                  <KebabMenu product={product}></KebabMenu>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </Table>
+      <section className={styles.minHeight}>
+        <Table >
+          <thead>
+            <tr>
+              <th className={styles.theadTitle}>
+                NOMBRE DE PRODUCTO
+                <button className={styles.sortButton}>
+                  <Icon icon="carbon:chevron-sort" width="20" height="30" />{" "}
+                </button>
+              </th>
+              <th className={styles.theadTitle}>
+                SKU SIMPLE
+                <button className={styles.sortButton}>
+                  <Icon icon="carbon:chevron-sort" width="20" height="30" />{" "}
+                </button>
+              </th>
+              <th className={styles.theadTitle}>
+                SKU
+                <button className={styles.sortButton}>
+                  <Icon icon="carbon:chevron-sort" width="20" height="30" />{" "}
+                </button>
+              </th>
+              <th className={styles.theadTitle}>
+                UPC
+                <button className={styles.sortButton}>
+                  <Icon icon="carbon:chevron-sort" width="20" height="30" />{" "}
+                </button>
+              </th>
+              <th className={styles.theadTitle}>
+                PRECIO
+                <button className={styles.sortButton}>
+                  <Icon icon="carbon:chevron-sort" width="20" height="30" />{" "}
+                </button>
+              </th>
+              <th className={styles.theadTitle}>
+                FECHA DE CARGA
+                <button className={styles.sortButton}>
+                  <Icon icon="carbon:chevron-sort" width="20" height="30" />{" "}
+                </button>
+              </th>
+              <th className={styles.theadTitle}>
+                ESTADO
+                <button className={styles.sortButton}>
+                  <Icon icon="carbon:chevron-sort" width="20" height="30" />{" "}
+                </button>
+              </th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product, index) => {
+              return (
+                <tr key={index}>
+                  <td>{product.product_name}</td>
+                  <td>{product.simple_sku}</td>
+                  <td>{product.sku} </td>
+                  <td>{product.upc}</td>
+                  <td>{product.price}</td>
+                  <td>{Moment(new Date(product.created_at)).format('DD/MM/YYYY')}</td>
+                  <td>
+                    <button className={product.enable ? styles.publishedButton : styles.publishedYellowButton}>Publicado</button>
+                  </td>
+                  <td>
+                      <KebabMenu product={product}></KebabMenu>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      </section>
     </>
   );
 };
